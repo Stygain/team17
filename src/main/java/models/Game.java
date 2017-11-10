caac2bb6c1a2c3a5283a4f6771c8250ac9961512
Adam Barton <adamb6102@gmail.com>
2017-11-10 13:30:30 -0800

    Partial update so others can get data

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -14,22 +14,22 @@ public class Game {
     //public java.util.List<Card> deck = new ArrayList<>();
 
     //old
-    //public java.util.List<java.util.List<Card>> cols = new ArrayList<>(4);
+    public java.util.List<java.util.List<Card>> cols = new ArrayList<>(4);
 
     //OO update
     //public java.util.List<Column> cols;// = new ArrayList<>(4);
-    public java.util.List<java.util.List<Column>> cols2 = new ArrayList<>(1);
+    //public java.util.List<java.util.List<Column>> cols2 = new ArrayList<>(1);
 
     public Game(){
         // initialize a new game such that each column can store cards
-        cols2.add(new ArrayList<Column>(4));
+        /*cols2.add(new ArrayList<Column>(4));
         for (int i = 0; i < 4; i++) {
             cols2.get(0).add(new Column());
-        }
-        /*original
+        }*/
+        //original
         for (int i = 0; i < 4; i++){
             cols.add(new ArrayList<Card>(4));
-        }*/
+        }
         /*OO update
         for (int i = 0; i < 4; i++){
             cols.add(new Column());
@@ -66,11 +66,19 @@ public class Game {
         int cards = deck.getSize();
         if (cards >= 4){
             for(int i = 0; i < 4; i++) {
+                //OO update
+                //Card temp = deck.getTopCard();
+
+                //original
+//                Card temp = this.deck.get(this.deck.size()-1);// remove top card from deck
+//                this.addCardToCol(i, temp);//add card to column
+//                this.deck.remove(temp);//remove top card from deck
+
                 Card temp = deck.getTopCard();
-                //Card temp = this.deck.get(this.deck.size()-1);// remove top card from deck
-                cols2.get(0).get(i).addCard(temp);
-                //this.addCardToCol(i, temp);//add card to column
-                //this.deck.remove(temp);//remove top card from deck
+                this.addCardToCol(i, temp);
+
+                //OO update
+                // cols2.get(0).get(i).addCard(temp);
             }
         }
         else{
@@ -80,63 +88,68 @@ public class Game {
 
     public boolean remove(int columnNumber) {
         // remove the top card from the indicated column
-        if(cols2.get(0).get(columnNumber).hasCards()/*columnHasCards(columnNumber)*/) {
+        if(columnHasCards(columnNumber)) {
             for (int i = 0; i < 4; i++){
-                if(cols2.get(0).get(i).peekTop().suit == cols2.get(0).get(columnNumber).peekTop().suit
+                /*if(cols2.get(0).get(i).peekTop().suit == cols2.get(0).get(columnNumber).peekTop().suit
                         && cols2.get(0).get(i).peekTop().value > cols2.get(0).get(columnNumber).peekTop().value) {
                     cols2.get(0).get(columnNumber).removeCard();
                         return true;
-                }
-                /*if (this.cols.get(i).get(this.cols.get(i).size()-1).suit == this.cols.get(columnNumber).get(this.cols.get(columnNumber).size()-1).suit &&
+                }*/
+                if (this.cols.get(i).get(this.cols.get(i).size()-1).suit == this.cols.get(columnNumber).get(this.cols.get(columnNumber).size()-1).suit &&
                         this.cols.get(i).get(this.cols.get(i).size()-1).value > this.cols.get(columnNumber).get(this.cols.get(columnNumber).size()-1).value) {
                     removeCardFromCol(columnNumber);
-                }*/
+                }
             }
         }
         return false;
     }
-/*
+
     private boolean columnHasCards(int columnNumber) {
         // check indicated column for number of cards; if no cards return false, otherwise return true
-       /* if(cols.get(columnNumber).hasCards()) {
+        //OO update
+        /*if(cols.get(columnNumber).hasCards()) {
             return true;
         }
-        return false;
-//        if (this.cols.get(columnNumber).isEmpty()) {
-//            return false;
-//        }
-//        return true;
+        return false;*/
+        if (this.cols.get(columnNumber).isEmpty()) {
+            return false;
+        }
+        return true;
     }
-*/
-/*
-    private Card getTopCard(int columnNumber) {
-        //return this.cols.get(columnNumber).get(this.cols.get(columnNumber).size()-1);
 
-    }*/
+
+    private Card getTopCard(int columnNumber) {
+        return this.cols.get(columnNumber).get(this.cols.get(columnNumber).size()-1);
+    }
 
 
     public boolean move(int columnFrom, int columnTo) {
         // remove the top card from the columnFrom column, add it to the columnTo column
-        if (cols2.get(0).get(columnFrom).hasCards()/*columnHasCards(columnFrom)*/) {
-            if (cols2.get(0).get(columnTo).hasCards()/*columnHasCards(columnTo)*/ == false) {
-                Card temp = cols2.get(0).get(columnFrom).getTop();
-                        //getTopCard(columnFrom); //get the top card
-                cols2.get(0).get(columnTo).addCard(temp);
-                return true;
-                //addCardToCol(columnTo, temp); //add it to the new column
-                //removeCardFromCol(columnFrom); //remove from the from column
+        if (/*cols2.get(0).get(columnFrom).hasCards()*/columnHasCards(columnFrom)) {
+            for (int i = 0; i < 4; i++) {
+                if (/*cols2.get(0).get(columnTo).hasCards()*/columnHasCards(i) == false) {
+                    /*Card temp = cols2.get(0).get(columnFrom).getTop();
+                            //getTopCard(columnFrom); //get the top card
+                    cols2.get(0).get(columnTo).addCard(temp);
+                    return true;*/
+
+                    Card temp = getTopCard(columnFrom);
+                    addCardToCol(columnTo, temp); //add it to the new column
+                    removeCardFromCol(columnFrom); //remove from the from column
+                    return true;
+                }
             }
         }
         return false;
     }
 
     private void addCardToCol(int columnTo, Card cardToMove) {
-        //this.cols.get(columnTo).add(cardToMove);
-        cols2.get(0).get(columnTo).addCard(cardToMove);
+        this.cols.get(columnTo).add(cardToMove);
+        //cols2.get(0).get(columnTo).addCard(cardToMove);
     }
 
     private void removeCardFromCol(int colFrom) {
-        //this.cols.get(colFrom).remove(this.cols.get(colFrom).size()-1);
-        cols2.get(0).get(colFrom).removeCard();
+        this.cols.get(colFrom).remove(this.cols.get(colFrom).size()-1);
+        //cols2.get(0).get(colFrom).removeCard();
     }
 }
