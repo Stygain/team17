76b18c1fafa2b06c3322d5eee4256c82e4189533
kiffj <jason.l.kiff@gmail.com>
2017-11-26 18:27:24 -0800

    update to sprint4 example code

--- src/main/java/controllers/ApplicationController.java
+++ src/main/java/controllers/ApplicationController.java
@@ -1,19 +1,3 @@
-/**
- * Copyright (C) 2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package controllers;
 
 import models.Game;
@@ -24,27 +8,23 @@ import ninja.Results;
 import com.google.inject.Singleton;
 import ninja.params.PathParam;
 
+
 @Singleton
 public class ApplicationController {
 
     public Result index() {
         return Results.html().template("views/AcesUp/AcesUp.flt.html");
     }
-    
+
     public Result gameGet(){
-        System.out.println("gameget");
         Game g = new Game();
-        //System.out.println("I'm past the constructor");
-        //g.deck.buildDeck();
-        //g.deck.shuffle();
+        g.deck.shuffle();
         g.dealFour();
-        //System.out.println("I'm at the return statement");
+
         return Results.json().render(g);
     }
 
     public Result dealPost(Context context, Game g) {
-
-        System.out.println("calling dealPost");
         if(context.getRequestPath().contains("deal")){
             g.dealFour();
         }
@@ -52,19 +32,13 @@ public class ApplicationController {
     }
 
     public Result removeCard(Context context, @PathParam("column") int colNumber, Game g){
-
-        System.out.println("calling removeCard");
-        boolean removed = false;
-        removed = g.remove(colNumber);
-        if (!removed){
-            removed = g.move(colNumber, 0);
-        }
-        return Results.json().render(g);
+        g.remove(colNumber);
+        return  Results.json().render(g);
     }
 
     public Result moveCard(Context context, @PathParam("columnFrom") int colFrom, @PathParam("columnTo") int colTo, Game g){
         g.move(colFrom,colTo);
-        return Results.json().render(g);
+        return  Results.json().render(g);
     }
 
 }
