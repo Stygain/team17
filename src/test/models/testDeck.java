9f75f7f603c4836ae0341f8857d88d1579085213
Adam Barton <adamb6102@gmail.com>
2017-12-02 12:31:40 -0800

    Added most assertions

--- src/test/models/testDeck.java
+++ src/test/models/testDeck.java
@@ -2,17 +2,31 @@ package models;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
 public class testDeck {
 
     @Test
-    public void testShuffle() {
+    public void testNewDeck() {
         Deck d = new Deck();
-        d.shuffle();
+        assertNotNull(d);
     }
     @Test
-    public void testDealFour() {
+    public void testDeckBuilt() {
         Deck d = new Deck();
-        d.dealFour();
+        assertEquals(52, d.cards.size());
+    }
+    @Test
+    public void testShuffle() {
+        Deck d1 = new Deck();
+        d1.shuffle();
+        Deck d2 = new Deck();
+        d2.shuffle();
+        assertFalse(Arrays.equals(d1.cards.toArray(), d2.cards.toArray()));
     }
 
 }
